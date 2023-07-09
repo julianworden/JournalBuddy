a18db4a1f42f9181c780d3d96a74740a28cfede1
Julian Worden <93101937+julianworden@users.noreply.github.com>
2023-07-09 10:26:07 -0700

    Remove Storyboard and Set Up Programmatic UI

--- JournalBuddy/HomeViewController.swift
+++ JournalBuddy/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  JournalBuddy
 //
 //  Created by Julian Worden on 7/9/23.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        view.backgroundColor = .systemBackground
     }
-
-
 }
 
