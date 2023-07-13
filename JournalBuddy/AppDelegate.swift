5ffc6387b22e287c95dfa277ad422127ac285ae2
Julian Worden <93101937+julianworden@users.noreply.github.com>
2023-07-13 06:10:25 -0700

    Remove Remaining Firebase Code

--- JournalBuddy/AppDelegate.swift
+++ JournalBuddy/AppDelegate.swift
@@ -6,13 +6,11 @@
 //
 
 import FirebaseAnalytics
-import FirebaseCore
 import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        FirebaseApp.configure()
         return true
     }
 
