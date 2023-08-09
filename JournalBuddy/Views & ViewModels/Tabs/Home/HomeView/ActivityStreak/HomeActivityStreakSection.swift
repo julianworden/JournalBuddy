fd2ed5d05481b779dc4414b71788538ac34f8f8e
Julian Worden <93101937+julianworden@users.noreply.github.com>
2023-08-09 14:51:49 -0700

    Refactor HomeActivityStreakSection's Secondary Box
    Created HomeSectionSecondaryBox. This view will be reused throughout HomeView to show data accompanied by an icon.

--- JournalBuddy/Views & ViewModels/Tabs/Home/HomeView/ActivityStreak/HomeActivityStreakSection.swift
+++ JournalBuddy/Views & ViewModels/Tabs/Home/HomeView/ActivityStreak/HomeActivityStreakSection.swift
@@ -11,12 +11,9 @@ import UIKit
 class HomeActivityStreakSection: UIView {
     private lazy var primaryBackgroundBox = UIView()
     private lazy var titleLabel = UILabel()
-    private lazy var primaryBoxContentStack = UIStackView(arrangedSubviews: [streakLabel, secondaryBackgroundBox])
+    private lazy var primaryBoxContentStack = UIStackView(arrangedSubviews: [streakLabel, secondaryBox])
     private lazy var streakLabel = UILabel()
-    private lazy var secondaryBackgroundBox = UIView()
-    private lazy var secondaryBoxContentStack = UIStackView(arrangedSubviews: [streakNumberIcon, streakNumberLabel])
-    private lazy var streakNumberIcon = UIImageView(image: UIImage(systemName: "calendar", withConfiguration: .primaryElementColor))
-    private lazy var streakNumberLabel = UILabel()
+    private lazy var secondaryBox = HomeSectionSecondaryBox(iconName: "calendar", text: "5 Day Streak")
 
     var cancellables = Set<AnyCancellable>()
 
@@ -51,27 +48,11 @@ class HomeActivityStreakSection: UIView {
         streakLabel.font = UIFontMetrics.avenirNextRegularBody
         streakLabel.textColor = .primaryElement
         streakLabel.numberOfLines = 0
-
-        secondaryBackgroundBox.backgroundColor = .background
-        secondaryBackgroundBox.layer.cornerRadius = 15
-        secondaryBackgroundBox.layer.shadowOpacity = 0.2
-        secondaryBackgroundBox.layer.shadowOffset = CGSize(width: 0, height: 2)
-
-        secondaryBoxContentStack.axis = .vertical
-
-        streakNumberIcon.contentMode = .scaleAspectFit
-
-        streakNumberLabel.text = "5 Day Streak"
-        streakNumberLabel.textColor = .primaryElement
-        streakNumberLabel.textAlignment = .center
-        streakNumberLabel.numberOfLines = 0
-        streakNumberLabel.font = UIFontMetrics.avenirNextRegularBody
     }
 
     func makeAccessible() {
         titleLabel.adjustsFontForContentSizeCategory = true
         streakLabel.adjustsFontForContentSizeCategory = true
-        streakNumberLabel.adjustsFontForContentSizeCategory = true
 
         adjustLayoutIfNeeded()
     }
@@ -101,7 +82,6 @@ class HomeActivityStreakSection: UIView {
     func constrain() {
         addConstrainedSubviews(titleLabel, primaryBackgroundBox)
         primaryBackgroundBox.addConstrainedSubviews(primaryBoxContentStack)
-        secondaryBackgroundBox.addConstrainedSubview(secondaryBoxContentStack)
 
         NSLayoutConstraint.activate([
             titleLabel.topAnchor.constraint(equalTo: topAnchor),
@@ -117,15 +97,6 @@ class HomeActivityStreakSection: UIView {
             primaryBoxContentStack.bottomAnchor.constraint(equalTo: primaryBackgroundBox.bottomAnchor, constant: -10),
             primaryBoxContentStack.leadingAnchor.constraint(equalTo: primaryBackgroundBox.leadingAnchor, constant: 10),
             primaryBoxContentStack.trailingAnchor.constraint(equalTo: primaryBackgroundBox.trailingAnchor, constant: -10),
-
-            secondaryBoxContentStack.topAnchor.constraint(equalTo: secondaryBackgroundBox.topAnchor, constant: 5),
-            secondaryBoxContentStack.bottomAnchor.constraint(equalTo: secondaryBackgroundBox.bottomAnchor, constant: -5),
-            secondaryBoxContentStack.leadingAnchor.constraint(equalTo: secondaryBackgroundBox.leadingAnchor, constant: 10),
-            secondaryBoxContentStack.trailingAnchor.constraint(equalTo: secondaryBackgroundBox.trailingAnchor, constant: -10),
-            secondaryBoxContentStack.widthAnchor.constraint(greaterThanOrEqualToConstant: 75),
-
-            streakNumberIcon.heightAnchor.constraint(greaterThanOrEqualToConstant: 30),
-            streakNumberIcon.widthAnchor.constraint(greaterThanOrEqualToConstant: 30)
         ])
     }
 }
---
+++ JournalBuddy/Views & ViewModels/Tabs/Home/HomeView/Reusable UIViews/HomeSectionSecondaryBox.swift
@@ -0,0 +1,73 @@
+//
+//  HomeSectionSecondaryBox.swift
+//  JournalBuddy
+//
+//  Created by Julian Worden on 8/9/23.
+//
+
+import UIKit
+
+class HomeSectionSecondaryBox: UIView {
+    private lazy var contentStack = UIStackView(arrangedSubviews: [iconImageView, textLabel])
+    private lazy var iconImageView = UIImageView(image: UIImage(systemName: iconName, withConfiguration: .primaryElementColor))
+    private lazy var textLabel = UILabel()
+
+    let iconName: String
+    let text: String
+
+    init(iconName: String, text: String) {
+        self.iconName = iconName
+        self.text = text
+
+        super.init(frame: .zero)
+
+        configure()
+        makeAccessible()
+        constrain()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+
+        layer.cornerRadius = 15
+        layer.shadowOpacity = 0.2
+        layer.shadowOffset = CGSize(width: 0, height: 2)
+    }
+
+    func configure() {
+        backgroundColor = .background
+
+        contentStack.axis = .vertical
+
+        textLabel.text = text
+        textLabel.font = UIFontMetrics.avenirNextRegularBody
+        textLabel.textColor = .primaryElement
+        textLabel.textAlignment = .center
+        textLabel.numberOfLines = 0
+
+        iconImageView.contentMode = .scaleAspectFit
+    }
+
+    func makeAccessible() {
+        textLabel.adjustsFontForContentSizeCategory = true
+    }
+
+    func constrain() {
+        addConstrainedSubviews(contentStack)
+
+        NSLayoutConstraint.activate([
+            contentStack.topAnchor.constraint(equalTo: topAnchor, constant: 5),
+            contentStack.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -5),
+            contentStack.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10),
+            contentStack.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10),
+            contentStack.widthAnchor.constraint(greaterThanOrEqualToConstant: 50),
+
+            iconImageView.heightAnchor.constraint(greaterThanOrEqualToConstant: 30),
+            iconImageView.widthAnchor.constraint(greaterThanOrEqualToConstant: 30)
+        ])
+    }
+}
